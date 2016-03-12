823a96311a1f984c6e22b6e084e2c9beb2ca3f05
zakorra <svenp666@gmail.com>
2016-03-12 12:26:19 +0100

    menu

---
+++ Assets/Scripts/ToggleProbeManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ToggleProbeManager : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void toggleCanvas() {
+        if (!gameObject.activeSelf) {
+            gameObject.SetActive(true);
+        } else {
+            gameObject.SetActive(false);
+        }
+    }
+}
